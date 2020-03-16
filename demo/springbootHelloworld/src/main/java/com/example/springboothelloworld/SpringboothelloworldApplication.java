f0d654b552d7dd3be671e5cab5379cbbb3278e73
huyh <591673663@qq.com>
2020-03-16 16:14:12 +0800

    init project

---
+++ demo/springbootHelloworld/src/main/java/com/example/springboothelloworld/SpringboothelloworldApplication.java
@@ -0,0 +1,16 @@
+package com.example.springboothelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Administrator
+ */
+@SpringBootApplication
+public class SpringboothelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringboothelloworldApplication.class, args);
+    }
+
+}
---
+++ demo/springbootHelloworld/src/main/java/com/huyh/Control/HelloControl.java
@@ -0,0 +1,17 @@
+package com.huyh.Control;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Administrator
+ * Date: 2020/3/16 Time: 15:00
+ * To change this template use File | Settings | File Templates.
+ * Description:
+ */
+@RestController
+public class HelloControl {
+
+
+
+}
